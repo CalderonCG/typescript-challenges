29603d6513eb13ca8ca67f230495b3780d23ef30
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 14:03:51 -0600

    feat: add solution for last

--- questions/10-medium-tuple-to-union/template.ts
+++ questions/10-medium-tuple-to-union/template.ts
@@ -1,3 +1,4 @@
+//Destructures the array and adds the first element into the union, then recursively calls itself with the rest of the array
 export type TupleToUnion<T> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest> : never
 
 type Arr = ['1', '2', '3']
--- questions/15-medium-last/template.ts
+++ questions/15-medium-last/template.ts
@@ -1,1 +1,11 @@
-export type Last<T extends any[]> = any
+
+//Verifies if there's only one element in the array
+//If there are more than 1 then pops the first element a recursively calls itself with the rest of the array
+export type Last<T extends any[]> = T extends [infer Only] ? Only : T extends [infer First, ...infer Rest] ? Last<Rest> : never 
+
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type tail1 = Last<arr1> // expected to be 'c'
+type tail2 = Last<arr2> // expected to be 1
