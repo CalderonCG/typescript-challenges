c435869206775cd0e00bd6cdf9b86162d27369bd
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 13:40:00 -0600

    feat: add solution for deep readonly

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,31 @@
-export type DeepReadonly<T> = any
+
+//Verifies if T is a function or not an object, if that's true then it returns the same T
+//If T is an object then it recursively calls itself on each element of the object while making that element readonly
+export type DeepReadonly<T> = T extends (...args: any[]) => any
+  ? T
+  : T extends object
+    ? {
+        readonly [K in keyof T]: DeepReadonly<T[K]>
+      }
+    : T;
+
+
+
+type X = {
+  x: {
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+
+type Expected = {
+  readonly x: {
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey'
+}
+
+type todo= DeepReadonly<X> // should be same as `Expected`
+
