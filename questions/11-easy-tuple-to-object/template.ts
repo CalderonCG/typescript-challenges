ecc31380bde3729ce40bf747d10fb6fa70dd48fc
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 08:40:03 -0600

    feat: add solution for readonly and exclude

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,10 @@
-export type TupleToObject<T extends readonly any[]> = any
+export type TupleToObject<T extends readonly any[]> = {
+    [K in keyof T as K] : T[K]
+}
+
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+const typeAux = keyof T
+
+const result: TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
--- questions/7-easy-readonly/template.ts
+++ questions/7-easy-readonly/template.ts
@@ -1,1 +1,8 @@
-export type MyReadonly<T> = any
+export type MyReadonly<T> = {
+    readonly [K in keyof T] : T[K]
+}
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+}
