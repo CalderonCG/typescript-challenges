9564c6cd2fba6c16f63e44e73a49d8e4eb57adef
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 17:54:59 -0600

    feat: add solution for string to union

--- questions/531-medium-string-to-union/template.ts
+++ questions/531-medium-string-to-union/template.ts
@@ -1,1 +1,8 @@
-export type StringToUnion<T extends string> = any
+//Destructure the string into the first character and the rest
+//Then recursively call StringToUnion with the rest
+export type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`? First | StringToUnion<Rest> : never
+
+
+
+type Test = '123'
+type Result = StringToUnion<Test> // expected to be "1" | "2" | "3"
