f37eda0756c406dc7200d18dbf6defe321664b9f
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 16:02:49 -0600

    feat: add solution for replace all

--- questions/116-medium-replace/template.ts
+++ questions/116-medium-replace/template.ts
@@ -4,7 +4,7 @@ export type Replace<
   To extends string
 > = From extends ''? S : //Checks if from is an empty string, if so then returns S
 S extends `${infer text}${From}${infer rest}` ? //Restructures S to extract the From
-`${text}${To}${rest}` : '' //Replaces From with To
+`${text}${To}${rest}` : S //Replaces From with To
 
 
 type replaced = Replace<'types are fun!', 'fun', 'awesome'> // expected to be 'types are awesome!'
--- questions/119-medium-replaceall/template.ts
+++ questions/119-medium-replaceall/template.ts
@@ -2,4 +2,7 @@ export type ReplaceAll<
   S extends string,
   From extends string,
   To extends string
-> = any
+> =From extends ''? S : //Checks if from is an empty string, if so then returns S
+S extends `${infer text}${From}${infer rest}` ? //Restructures S to extract the From
+`${text}${To}${ReplaceAll<rest, From, To>}` //Replaces From with To and recursively calls itself with the rest of the string
+: S 
