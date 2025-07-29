2f0f26d129ea1a83d5f7c1c8978d5c224a9e81a0
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 09:30:41 -0600

    feat: add solution for first and concat

--- questions/14-easy-first/template.ts
+++ questions/14-easy-first/template.ts
@@ -1,1 +1,8 @@
-export type First<T extends any[]> = any
+export type First<T extends any[]> = T extends [infer First, ... infer Rest] ? First : never
+
+
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
--- questions/533-easy-concat/template.ts
+++ questions/533-easy-concat/template.ts
@@ -1,1 +1,1 @@
-export type Concat<T, U> = any
+export type Concat<T, U> = T extends [...infer RestT] ? U extends [...infer RestU] ? [...RestT, ...RestU] : [] : []
