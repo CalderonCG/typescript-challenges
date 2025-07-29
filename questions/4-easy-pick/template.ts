af81b20aa715897f79878eb844daa05829bd2798
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 08:19:23 -0600

    feat: add easy pick solution

--- questions/4-easy-pick/template.ts
+++ questions/4-easy-pick/template.ts
@@ -1,1 +1,11 @@
-export type MyPick<T, K> = any
+export type MyPick<T, K extends keyof T> = {
+    [key in keyof T as key extends K? key: never ]: T[key]
+}
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
