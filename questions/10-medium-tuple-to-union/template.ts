91ba7c32f5434fb825918059baa1652f5203c0bd
CalderonCodes <cgcalderondev@gmail.com>
2025-07-29 13:43:05 -0600

    feat: add solution for tuple to union

--- questions/10-medium-tuple-to-union/template.ts
+++ questions/10-medium-tuple-to-union/template.ts
@@ -1,1 +1,5 @@
-export type TupleToUnion<T> = any
+export type TupleToUnion<T> = T extends [infer First, ...infer Rest] ? First | TupleToUnion<Rest> : never
+
+type Arr = ['1', '2', '3']
+
+type a= TupleToUnion<Arr> // expected to be '1' | '2' | '3'
