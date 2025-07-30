7347f1b403899e00fa33d5cd23a5774954f4d54c
CalderonCodes <cgcalderondev@gmail.com>
2025-07-30 11:08:43 -0600

    feat: add solution for diff

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,5 +1,18 @@
+//Compares the keys and if there share it then assigns it never to remove it
 export type Diff<O, O1> = {
-    [k in keyof O as k extends in keyof O1 ? :]
+    [k in keyof O1 as k extends keyof O ? never:k] : O1[k]
 }
+
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+
+type mergeDiff = Diff<Foo,Bar>
 
 type cases = [Expect<Equal<Diff<Foo, Bar>, { gender: number }>>]
