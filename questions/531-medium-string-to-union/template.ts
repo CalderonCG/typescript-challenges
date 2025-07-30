027e118a9bfac2addbb9b2fa993265ccb31206b6
CalderonCodes <cgcalderondev@gmail.com>
2025-07-30 09:27:39 -0600

    feat: add solution for camelcase

--- questions/531-medium-string-to-union/template.ts
+++ questions/531-medium-string-to-union/template.ts
@@ -2,7 +2,5 @@
 //Then recursively call StringToUnion with the rest
 export type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}`? First | StringToUnion<Rest> : never
 
-
-
 type Test = '123'
 type Result = StringToUnion<Test> // expected to be "1" | "2" | "3"
--- questions/610-medium-camelcase/template.ts
+++ questions/610-medium-camelcase/template.ts
@@ -1,1 +1,24 @@
-export type CamelCase<S> = any
+// export type CamelCase<S> = S extends `${infer Head}${infer Dash}${infer Tail}` ?
+// Dash extends `-` ? 
+// Tail extends `${infer First}${infer Rest}` ? `${Head}${Uppercase<First>}${Rest}`:'':CamelCase<`${Head}${CamelCase<`${Dash}${Tail}`>}`>:''
+
+
+// export type CamelCase<S> = S extends `${infer Head}${infer Dash}${infer Tail}` ?  //Destructure the string into the first two chars and the rest
+// //Checks if Dash is -, then checks if there are more - after it or if there is a normal string
+// Dash extends `-` ? Tail extends `${infer First}${infer Rest}` ? First extends `-`?  `${Head}${Dash}${CamelCase<`${Rest}`>}`:
+//  `${Head}${CamelCase<`${Uppercase<First>}${Rest}`>}`:
+// Uppercase<S>
+// :`${Head}${CamelCase<`${Dash}${Tail}`>}`:S
+//Logic got too complex, let's refactor
+
+//Extends checks explicitly for the - now
+export type CamelCase<S>= S extends `${infer Head}-${infer Next}${infer Rest}`
+    ? Next extends '-' //Checks if after the first - there is another -
+      ? `${Head}-${CamelCase<`-${Rest}`>}` //If there is then it doesnt remove the first -
+      //Checks if Dash is lower or uppercase, if it is uppercase then adds the - back
+      //If it is lowercase then proceeds with the camelcase iteration 
+      : `${Head}${Next extends Lowercase<Next> ? Uppercase<Next> : `-${Next}`}${CamelCase<Rest>}`
+    : S;
+
+
+type camel = CamelCase<'foo-Bar-Baz'>
