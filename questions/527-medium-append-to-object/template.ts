c163f13fd4ca606bf86fae928fb87f89f55e681f
CalderonCodes <cgcalderondev@gmail.com>
2025-07-30 11:33:35 -0600

    feat: add solution for merge

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,5 +1,8 @@
-export type AppendToObject<T, U extends string|number|symbol, V> = T & {
-    [K in U]: V
+export type AppendToObject<T, U extends string|number|symbol, V> =  {
+//Checks K in T or takes U as keys
+//If the K was in T then returns that element
+//Else it adds the V value
+  [K in keyof T | U]: K extends keyof T ? T[K] : V
 }
 
 
