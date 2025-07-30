557f82834d933c7b05bfe7c9077cdc1e88b9ba99
CalderonCodes <cgcalderondev@gmail.com>
2025-07-30 11:27:23 -0600

    feat: add solution for merge

--- questions/599-medium-merge/template.ts
+++ questions/599-medium-merge/template.ts
@@ -1,1 +1,14 @@
-export type Merge<F, S> = any
+export type Merge<F, S> = {
+    //Remove the K of F that appear in S, and take the remaining keys + the S keys
+    [K in keyof (Omit<F, keyof S> & S)]: (Omit<F, keyof S> & S)[K]
+}
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+}
+
+type merged = Merge<Foo, Bar>
