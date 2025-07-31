449a62e3da7a8be9f76c8645bbe7774b23f9f182
CalderonCodes <cgcalderondev@gmail.com>
2025-07-31 09:33:50 -0600

    feat: add solution for get required and get optional

--- questions/57-hard-get-required/template.ts
+++ questions/57-hard-get-required/template.ts
@@ -1,1 +1,15 @@
-export type GetRequired<T> = any
+//Compares each key to the same type with all the required keys
+//This case if one isnt required the comparison fails, and returns never, removing that key
+export type GetRequired<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+}
+
+type OmitByType<Type, Omited> = {
+  //Gets 2 Types
+  //If Type === Omited Type then the KEY gets reeplaced to never
+  [K in keyof Type as Type[K] extends Omited ? never : K]: Type[K]
+}
+
+
+
+type I = GetRequired<{ foo: number; bar?: string }> // expected to be { foo: number }
--- questions/59-hard-get-optional/template.ts
+++ questions/59-hard-get-optional/template.ts
@@ -1,1 +1,5 @@
-export type GetOptional<T> = any
+//Exactly the same logic of get required, but switching the return cases
+//This case if one is required then the comparison succeeds and returns never, removing that key
+export type GetOptional<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K]
+}
