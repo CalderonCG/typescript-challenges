e2998806b3d515adec67cea9cc06ab83cb05313b
CalderonCodes <cgcalderondev@gmail.com>
2025-07-31 13:13:24 -0600

    feat: add solution for tuple filter

--- questions/399-hard-tuple-filter/template.ts
+++ questions/399-hard-tuple-filter/template.ts
@@ -1,13 +1,20 @@
-export type _FilterOut<T extends any[], F> = T extends [infer First, ...infer Rest] ? 
-First extends F? [_FilterOut<Rest,F>] : [First, _FilterOut<Rest,F>] : T
+// export type FilterOuts<T extends any[], F>= Flatten<_FilterOut<T, F>>
 
-export type FilterOut<T extends any[], F>= Flatten<_FilterOut<T, F>>
+// export type Flatten<T extends any[]> = 
+//   T extends [infer First, ...infer Rest] //Destructures into first element and the rest
+//     ? First extends unknown[] //If first element is an array, recursively calls itself again
+//       ? [...Flatten<First>, ...Flatten<Rest>]
+//       : [First, ...Flatten<Rest>] //Else it calls itself with the rest
+//     : []
 
-export type Flatten<T extends any[]> = 
-  T extends [infer First, ...infer Rest] //Destructures into first element and the rest
-    ? First extends unknown[] //If first element is an array, recursively calls itself again
-      ? [...Flatten<First>, ...Flatten<Rest>]
-      : [First, ...Flatten<Rest>] //Else it calls itself with the rest
-    : []
+//This one does the same withot needing flatten
+export type FilterOut<T extends any[], F> =
+  T extends [infer First, ...infer Rest] //Destructures the array
+    ? [First] extends [F] //In case F is an union
+      ? FilterOut<Rest, F> //Recursively calls itself without the first element
+      : [First, ...FilterOut<Rest, F>] //Recursively calls itself while keeping the first element
+    : [];
 
-type Filtered = Flatten<FilterOut<[1, 2, null, 3, null], null>> // [1, 2, 3]
+
+
+type Filtered = FilterOut<[never], never> // [1, 2, 3]
