f7e9f88d8b0499692bc2bef5865fe0509b3bb6ce
CalderonCodes <cgcalderondev@gmail.com>
2025-07-31 11:19:18 -0600

    feat: add solution for string to number

--- questions/300-hard-string-to-number/template.ts
+++ questions/300-hard-string-to-number/template.ts
@@ -1,1 +1,5 @@
-export type ToNumber<S extends string> = any
+//Infered values can be extended to ensure a type
+export type ToNumber<S extends string> = S extends `${infer X extends number}`? X :''
+
+
+type num = ToNumber<'3'>//expected 3
--- questions/399-hard-tuple-filter/template.ts
+++ questions/399-hard-tuple-filter/template.ts
@@ -1,1 +1,13 @@
-export type FilterOut<T extends any[], F> = any
+export type _FilterOut<T extends any[], F> = T extends [infer First, ...infer Rest] ? 
+First extends F? [_FilterOut<Rest,F>] : [First, _FilterOut<Rest,F>] : T
+
+export type FilterOut<T extends any[], F>= Flatten<_FilterOut<T, F>>
+
+export type Flatten<T extends any[]> = 
+  T extends [infer First, ...infer Rest] //Destructures into first element and the rest
+    ? First extends unknown[] //If first element is an array, recursively calls itself again
+      ? [...Flatten<First>, ...Flatten<Rest>]
+      : [First, ...Flatten<Rest>] //Else it calls itself with the rest
+    : []
+
+type Filtered = Flatten<FilterOut<[1, 2, null, 3, null], null>> // [1, 2, 3]
