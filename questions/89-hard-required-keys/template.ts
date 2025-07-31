1f41297b8f5f74c55370f027af79a5b340467707
CalderonCodes <cgcalderondev@gmail.com>
2025-07-31 09:48:56 -0600

    feat: add solution for required keys and optional keys

--- questions/89-hard-required-keys/template.ts
+++ questions/89-hard-required-keys/template.ts
@@ -1,1 +1,14 @@
-export type RequiredKeys<T> = any
+//Compares each key to the same type with all the required keys
+//This case if one isnt required the comparison fails, and returns never, removing that key
+//And gets the keys of the resulting object
+export type RequiredKeys<T> = keyof {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+}
+
+//Use the type to get 
+export type GetRequired<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+}
+
+type Result = RequiredKeys<{ foo: number; bar?: string }>
+// expected to be “foo”
--- questions/90-hard-optional-keys/template.ts
+++ questions/90-hard-optional-keys/template.ts
@@ -1,1 +1,6 @@
-export type OptionalKeys<T> = any
+//Compares each key to the same type with all the required keys
+//This case if one is required the comparison succeeds, and returns never, removing that key
+//And gets the keys of the resulting object
+export type OptionalKeys<T> = keyof {
+    [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K]
+}
