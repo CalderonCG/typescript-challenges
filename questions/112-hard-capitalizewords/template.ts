8a29693c9f07ed036b914ce5a7301020ee86c276
CalderonCodes <cgcalderondev@gmail.com>
2025-07-31 10:43:29 -0600

    feat: add solution for capitalize words

--- questions/112-hard-capitalizewords/template.ts
+++ questions/112-hard-capitalizewords/template.ts
@@ -1,1 +1,13 @@
-export type CapitalizeWords<S extends string> = any
+// export type CapitalizeWords<S extends string> = S extends `${infer First}${` `|`,`|`.`}${infer Rest}`?
+//  `${Capitalize<First>} ${CapitalizeWords<Rest>}`:Capitalize<S>
+
+//K works as a flag to check what action to process
+//If the first read character is a blank space then that k flag is set to true, meaning the next first read char will be uppercased
+//If the first read character is a normal char then k is set to false, meaning the next first character wont be uppercased
+//This way the uppercase only happens after ` `|`,`|`.`
+export type CapitalizeWords<S extends string, K extends boolean = true>=
+S extends `${infer First}${infer Rest}` ?
+First extends ` `|`,`|`.` ? `${First}${CapitalizeWords<Rest, true>}`:
+ K extends true?  `${Uppercase<First>}${CapitalizeWords<Rest, false>}`:`${First}${CapitalizeWords<Rest, false>}`:''
+
+type capitalized = CapitalizeWords<'hello world, my friends'> // expected to be 'Hello World, My Friends'
