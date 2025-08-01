1920cb6e3626107d1a3f8c23f12da082c0fde9fa
CalderonCodes <cgcalderondev@gmail.com>
2025-08-01 11:30:48 -0600

    feat: add solution for string lenght 2

--- questions/651-hard-length-of-string-2/template.ts
+++ questions/651-hard-length-of-string-2/template.ts
@@ -1,1 +1,19 @@
-export type LengthOfString<S extends string> = number
+
+//Same logic as the medium one, we neede to transform into a tuple first
+type StringToTuple<
+  S extends string,
+  Acc extends unknown[] = []//Stores the array, accumulating it to reduce recursions
+> = S extends `${infer _First}${infer Rest}` 
+  ? StringToTuple<Rest, [unknown, ...Acc]> //Uses that accumulator for the recursions
+  : Acc; 
+
+  
+//With the string as a tuple it can use the lenght tuple value 
+export type LengthOfString<S extends string> = StringToTuple<S>["length"];
+
+
+
+
+type split<T extends unknown[]>= T extends [infer First, ...infer Last] ? T[2] : []
+
+type a= split<[1,2,3,4]>
